aad6e16bbf2780498784eb8f82b8b03ab904e04e
Tadatoshi Takahashi <tadatoshi.3.takahashi@gmail.com>
2013-02-02 16:05:26 -0500

    Added checking the state.

--- lib/arduino_sketch_builder/arduino_cmake_build.rb
+++ lib/arduino_sketch_builder/arduino_cmake_build.rb
@@ -2,35 +2,66 @@ require "open3"
 
 class ArduinoSketchBuilder::ArduinoCmakeBuild
 
-  STATES = [:initial, :cmake_complete, :make_complete, :make_upload_complete]
+  State = Struct.new(:value, :message)
 
-  attr_accessor :state
+  INITIAL = State.new(:initial, "ready") 
+  CMAKE_COMPLETE = State.new(:cmake_complete, "success") 
+  MAKE_COMPLETE = State.new(:make_complete, "success")
+  MAKE_UPLOAD_COMPLETE = State.new(:make_upload_complete, "success")
 
   def initialize
-    @state = :initial
+    @state = INITIAL
+  end
+
+  def state
+    @state.value
+  end
+
+  def message
+    @state.message
   end
 
   def cmake(build_directory, main_directory)
+
+    unless @state == INITIAL
+      raise "Wrong state '#{self.state}': can't call cmake when the state is not '#{INITIAL.value}'"
+    end
 
     Dir.chdir(build_directory)
 
     Open3.popen3("cmake #{main_directory}") do |stdin, stdout, stderr, wait_thread|
       pid = wait_thread.pid
       exit_status = wait_thread.value
-      @state = :cmake_complete if exit_status.success?
+      if exit_status.success?
+        @state = CMAKE_COMPLETE
+      else
+        @state = State.new(:cmake_incomplete, stderr.gets)
+      end
     end
+
+    self.state
 
   end
 
   def make(build_directory)
+
+    unless @state == CMAKE_COMPLETE
+      raise "Wrong state '#{self.state}': can't call make when the state is not '#{CMAKE_COMPLETE.value}'"
+    end    
 
     Dir.chdir(build_directory)
 
     Open3.popen3("make") do |stdin, stdout, stderr, wait_thread|
       pid = wait_thread.pid
       exit_status = wait_thread.value
-      @state = :make_complete if exit_status.success?
+      if exit_status.success?
+        @state = MAKE_COMPLETE
+      else
+        @state = State.new(:make_incomplete, stderr.gets)
+      end
     end    
+
+    self.state
 
   end
 
--- spec/arduino_sketch_builder/arduino_cmake_build_spec.rb
+++ spec/arduino_sketch_builder/arduino_cmake_build_spec.rb
@@ -19,18 +19,60 @@ describe ArduinoSketchBuilder::ArduinoCmakeBuild do
     @arduino_cmake_build.state.should == :initial
     File.exists?("#{BUILD_DIRECTORY}/Makefile").should be_false
 
-    @arduino_cmake_build.cmake(BUILD_DIRECTORY, MAIN_DIRECTORY)
+    @arduino_cmake_build.cmake(BUILD_DIRECTORY, MAIN_DIRECTORY).should == :cmake_complete
 
     @arduino_cmake_build.state.should == :cmake_complete
     File.exists?("#{BUILD_DIRECTORY}/Makefile").should be_true
 
     File.exists?("#{BUILD_DIRECTORY}/src/blink_customized_for_test.hex").should be_false
 
-    @arduino_cmake_build.make(BUILD_DIRECTORY)
+    @arduino_cmake_build.make(BUILD_DIRECTORY).should == :make_complete
 
     @arduino_cmake_build.state.should == :make_complete
     File.exists?("#{BUILD_DIRECTORY}/src/blink_customized_for_test.hex").should be_true
 
   end
+
+  it "should execute cmake only when the state is initial" do
+
+    @arduino_cmake_build.instance_eval("@state=CMAKE_COMPLETE")
+    expect { @arduino_cmake_build.cmake(BUILD_DIRECTORY, MAIN_DIRECTORY) }
+      .to raise_error("Wrong state 'cmake_complete': can't call cmake when the state is not 'initial'")
+    
+    @arduino_cmake_build.instance_eval("@state=MAKE_COMPLETE")
+    expect { @arduino_cmake_build.cmake(BUILD_DIRECTORY, MAIN_DIRECTORY) }
+      .to raise_error("Wrong state 'make_complete': can't call cmake when the state is not 'initial'")  
+
+    @arduino_cmake_build.instance_eval("@state=MAKE_UPLOAD_COMPLETE")
+    expect { @arduino_cmake_build.cmake(BUILD_DIRECTORY, MAIN_DIRECTORY) }
+      .to raise_error("Wrong state 'make_upload_complete': can't call cmake when the state is not 'initial'") 
+
+    @arduino_cmake_build.instance_eval("@state=INITIAL")
+    expect { @arduino_cmake_build.cmake(BUILD_DIRECTORY, MAIN_DIRECTORY) }
+      .to_not raise_error 
+
+  end
+
+  it "should execute make only when the state is cmake_complete" do
+
+    @arduino_cmake_build.instance_eval("@state=INITIAL")
+    expect { @arduino_cmake_build.make(BUILD_DIRECTORY) }
+      .to raise_error("Wrong state 'initial': can't call make when the state is not 'cmake_complete'")
+    
+    @arduino_cmake_build.instance_eval("@state=MAKE_COMPLETE")
+    expect { @arduino_cmake_build.make(BUILD_DIRECTORY) }
+      .to raise_error("Wrong state 'make_complete': can't call make when the state is not 'cmake_complete'")  
+
+    @arduino_cmake_build.instance_eval("@state=MAKE_UPLOAD_COMPLETE")
+    expect { @arduino_cmake_build.make(BUILD_DIRECTORY) }
+      .to raise_error("Wrong state 'make_upload_complete': can't call make when the state is not 'cmake_complete'") 
+
+    @arduino_cmake_build.instance_eval("@state=INITIAL")
+    @arduino_cmake_build.cmake(BUILD_DIRECTORY, MAIN_DIRECTORY)
+    @arduino_cmake_build.state.should == :cmake_complete
+    expect { @arduino_cmake_build.make(BUILD_DIRECTORY) }
+      .to_not raise_error 
+
+  end  
 
 end
