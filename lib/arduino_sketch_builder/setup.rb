b29c854b252c5b9a4287b1eee7de08f455d395d6
Tadatoshi Takahashi <tadatoshi.3.takahashi@gmail.com>
2013-02-13 11:51:39 -0500

    Code to copy the cmake files for Arduino under the specified directory.

--- lib/arduino_sketch_builder/setup.rb
+++ lib/arduino_sketch_builder/setup.rb
@@ -2,6 +2,19 @@ require 'active_support/core_ext/string/inflections'
 
 class ArduinoSketchBuilder::Setup
 
+  ARDUINO_CMAKE_DIRECTORY = File.expand_path('../../../arduino-cmake', __FILE__)
+
+  def configure(root_directory)
+
+    FileUtils.mkdir_p(File.join(root_directory, 'cmake', 'Platform'))
+
+    File.write(File.join(root_directory, 'cmake', 'ArduinoToolchain.cmake'), File.read(File.join(ARDUINO_CMAKE_DIRECTORY, 'cmake', 'ArduinoToolchain.cmake')))    
+    File.write(File.join(root_directory, 'cmake', 'Platform', 'Arduino.cmake'), File.read(File.join(ARDUINO_CMAKE_DIRECTORY, 'cmake', 'Platform', 'Arduino.cmake')))            
+
+    FileUtils.chmod_R('og-rwx', File.join(root_directory, 'cmake'))
+
+  end
+
   def setup(root_directory, sketch_file_path)
 
     sketch_file_name = File.basename(sketch_file_path)
@@ -9,15 +22,17 @@ class ArduinoSketchBuilder::Setup
   	main_directory_name = sketch_name.underscore
     
     FileUtils.mkdir_p([File.join(root_directory, main_directory_name), 
-    	               File.join(root_directory, main_directory_name, 'src'), 
-    	               File.join(root_directory, main_directory_name, 'build'), 
-    	               File.join(root_directory, main_directory_name, 'src', sketch_name) ])
+    	                 File.join(root_directory, main_directory_name, 'src'), 
+    	                 File.join(root_directory, main_directory_name, 'build'), 
+    	                 File.join(root_directory, main_directory_name, 'src', sketch_name)])
 
     c_make_lists_file_generator = ArduinoSketchBuilder::CMakeListsFileGenerator.new
     c_make_lists_file_generator.generate_main(root_directory, File.join(root_directory, main_directory_name))
     c_make_lists_file_generator.generate_sketch_specific(sketch_name, File.join(root_directory, main_directory_name, 'src'))
 
     File.write(File.join(root_directory, main_directory_name, 'src', sketch_name, sketch_file_name), File.read(sketch_file_path))    
+
+    FileUtils.chmod_R('og-rwx', File.join(root_directory, main_directory_name))
 
   end
 
--- spec/arduino_sketch_builder/setup_spec.rb
+++ spec/arduino_sketch_builder/setup_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ArduinoSketchBuilder::Setup do
 
+  ARDUINO_CMAKE_DIRECTORY = File.expand_path('../../../arduino-cmake', __FILE__)
   FIXTURES_DIRECTORY = File.expand_path('../../fixtures', __FILE__)
   ARDUINO_SKETCHES_FIXTURE_DIRECTORY = File.expand_path('../../arduino_sketches_fixture', __FILE__)
   ARDUINO_SKETCH_FILE_PATH = File.expand_path(File.join(ARDUINO_SKETCHES_FIXTURE_DIRECTORY, 'src/BlinkCustomizedForTest/BlinkCustomizedForTest.ino'), __FILE__)
@@ -12,26 +13,42 @@ describe ArduinoSketchBuilder::Setup do
     @setup = ArduinoSketchBuilder::Setup.new
   end
 
-  it "should configure the root directory by putting cmake directory and its contents from arduino-cmake submodule"
+  it "should configure the root directory by putting cmake directory and its contents from arduino-cmake submodule" do
+
+    root_directory = TEMP_DIRECTORY
+
+    @setup.configure(root_directory)
+
+    Dir.exists?(File.join(root_directory, "cmake")).should be_true 
+    File.exists?(File.join(root_directory, "cmake", "ArduinoToolchain.cmake")).should be_true
+    Dir.exists?(File.join(root_directory, "cmake", "Platform")).should be_true 
+    File.exists?(File.join(root_directory, "cmake", "Platform", "Arduino.cmake")).should be_true  
+    
+    File.read(File.join(root_directory, "cmake", "ArduinoToolchain.cmake")).should == File.read(File.join(ARDUINO_CMAKE_DIRECTORY, "cmake", "ArduinoToolchain.cmake"))    
+    File.read(File.join(root_directory, "cmake", "Platform", "Arduino.cmake")).should == File.read(File.join(ARDUINO_CMAKE_DIRECTORY, "cmake", "Platform", "Arduino.cmake"))    
+
+  end
 
   it "should create a directory structure for an Arduino sketch" do
 
-    @setup.setup(TEMP_DIRECTORY, ARDUINO_SKETCH_FILE_PATH)
+    root_directory = TEMP_DIRECTORY
 
-    Dir.exists?(File.join(TEMP_DIRECTORY, "blink_customized_for_test")).should be_true
-    File.exists?(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "CMakeLists.txt")).should be_true
-    Dir.exists?(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "src")).should be_true
-    File.exists?(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "src", "CMakeLists.txt")).should be_true
-    Dir.exists?(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "src", "BlinkCustomizedForTest")).should be_true
-    File.exists?(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "src", "BlinkCustomizedForTest", "BlinkCustomizedForTest.ino")).should be_true
-    Dir.exists?(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "build")).should be_true
+    @setup.setup(root_directory, ARDUINO_SKETCH_FILE_PATH)
+
+    Dir.exists?(File.join(root_directory, "blink_customized_for_test")).should be_true
+    File.exists?(File.join(root_directory, "blink_customized_for_test", "CMakeLists.txt")).should be_true
+    Dir.exists?(File.join(root_directory, "blink_customized_for_test", "src")).should be_true
+    File.exists?(File.join(root_directory, "blink_customized_for_test", "src", "CMakeLists.txt")).should be_true
+    Dir.exists?(File.join(root_directory, "blink_customized_for_test", "src", "BlinkCustomizedForTest")).should be_true
+    File.exists?(File.join(root_directory, "blink_customized_for_test", "src", "BlinkCustomizedForTest", "BlinkCustomizedForTest.ino")).should be_true
+    Dir.exists?(File.join(root_directory, "blink_customized_for_test", "build")).should be_true
 
     # The following checks the generated file with the full path, hence, system specific (e.g. /Users/tadatoshi). 
     # Hence, it's considered that the unit test for c_make_lists_file_generator covers it. 
-    # File.read(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "CMakeLists.txt")).should == File.read(File.join(FIXTURES_DIRECTORY, "MainCMakeLists.txt"))
+    # File.read(File.join(root_directory, "blink_customized_for_test", "CMakeLists.txt")).should == File.read(File.join(FIXTURES_DIRECTORY, "MainCMakeLists.txt"))
     # This one also, it's considered that the unit test for c_make_lists_file_generator covers it.
-    # File.read(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "src", "CMakeLists.txt")).should == File.read(File.join(FIXTURES_DIRECTORY, "CMakeListsForTestSketch.txt"))
-    File.read(File.join(TEMP_DIRECTORY, "blink_customized_for_test", "src", "BlinkCustomizedForTest", "BlinkCustomizedForTest.ino")).should == File.read(File.join(ARDUINO_SKETCHES_FIXTURE_DIRECTORY, "src", "BlinkCustomizedForTest", "BlinkCustomizedForTest.ino"))    
+    # File.read(File.join(root_directory, "blink_customized_for_test", "src", "CMakeLists.txt")).should == File.read(File.join(FIXTURES_DIRECTORY, "CMakeListsForTestSketch.txt"))
+    File.read(File.join(root_directory, "blink_customized_for_test", "src", "BlinkCustomizedForTest", "BlinkCustomizedForTest.ino")).should == File.read(File.join(ARDUINO_SKETCHES_FIXTURE_DIRECTORY, "src", "BlinkCustomizedForTest", "BlinkCustomizedForTest.ino"))    
 
   end
 
